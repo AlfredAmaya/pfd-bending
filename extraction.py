6d25335b5595f63ac15248dd183fa795d8c390b7
Alfredo Amaya <aamayac1600@alumno.ipn.mx>
2021-09-26 12:46:50 -0500

    bux fixing

--- extraction.py
+++ extraction.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-pdf_path = ( Path.home()/"Desktop"/"pdfmanipulator"/"hello2.pdf")
+pdf_path = ( Path.home()/"Desktop"/"pdfmanipulator"/"square.pdf")
 input_pdf=PdfFileReader(str(pdf_path))
-first_page = input_pdf.getPage(0)
+first_page = input_pdf.getPage(1)
 pdf_writer = PdfFileWriter()
 pdf_writer.addPage(first_page)
+with Path("green_square.pdf").open(mode="wb") as output_file:
+    pdf_writer.write(output_file)
+
