86e63a053adc87811324d71b8865b045104fe37a
Alfredo Amaya <aamayac1600@alumno.ipn.mx>
2021-09-25 23:28:54 -0500

    fixed a path error on 'extraction.py'

--- extraction.py
+++ extraction.py
@@ -3,3 +3,6 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 
 pdf_path = ( Path.home()/"Desktop"/"pdfmanipulator"/"hello2.pdf")
 input_pdf=PdfFileReader(str(pdf_path))
+first_page = input_pdf.getPage(0)
+pdf_writer = PdfFileWriter()
+pdf_writer.addPage(first_page)
