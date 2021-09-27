b3b97aa3dada2da8c02059a706a88fb010af8614
Alfredo Amaya <aamayac1600@alumno.ipn.mx>
2021-09-26 23:06:05 -0500

    experimenting with rotation function

--- multi_extraction.py
+++ multi_extraction.py
@@ -1,13 +1,15 @@
-from PyPDF2 import PdfFileReader, PdfFileWriter
 from pathlib import Path
 
-pdf_path = ( Path.home()/"Desktop"/"pdfmanipulator"/"network_setup_recorde.pdf")
+from PyPDF2 import PdfFileReader, PdfFileWriter 
+
+pdf_path = ( Path.home()/"Desktop"/"pdfmanipulator"/"network_setup_recorder.pdf")
 input_pdf = PdfFileReader(str(pdf_path))
 
 pdf_writer = PdfFileWriter()
-pdf_writer_2 = PdfFileWriter()
+
 for n in range(0, 2):
     page = input_pdf.getPage(n)
+    page.rotateClockwise(90*n)
     pdf_writer.addPage(page)
 
 with Path("chapter1.pdf").open(mode="wb") as output_file:
