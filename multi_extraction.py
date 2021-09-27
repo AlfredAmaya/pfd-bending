7935bc032968f14cf58eb138cc57d30db154fb17
Alfredo Amaya <aamayac1600@alumno.ipn.mx>
2021-09-27 00:39:32 -0500

    more rotation experiments

--- multi_extraction.py
+++ multi_extraction.py
@@ -7,12 +7,16 @@ input_pdf = PdfFileReader(str(pdf_path))
 
 pdf_writer = PdfFileWriter()
 
-for n in range(0, 2):
-    page = input_pdf.getPage(n)
-    page.rotateClockwise(90*n)
+#for n in range(0, 2):
+#    page = input_pdf.getPage(n)
+#    page.rotateClockwise(90*n)
+#    pdf_writer.addPage(page)
+for page in input_pdf.pages[0:4]:
+    page.rotateClockwise(90*input_pdf.getPageNumber(page))
     pdf_writer.addPage(page)
 
-with Path("chapter1.pdf").open(mode="wb") as output_file:
+
+with Path("chapter1slice.pdf").open(mode="wb") as output_file:
     pdf_writer.write(output_file)
 
 
