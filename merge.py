2b630c30ca3870754460cae7b44fa3849d33b201
Alfredo Amaya <aamayac1600@alumno.ipn.mx>
2021-09-30 12:24:19 -0500

    fully functinal experiment merge.py

--- merge.py
+++ merge.py
@@ -12,9 +12,6 @@ input_pathA = Path(file_selected.name)
 file_selected = filedialog.askopenfile()
 input_pathB = Path(file_selected.name)
 
-DocA = PdfFileReader(str(input_pathA))
-DocB = PdfFileReader(str(input_pathB))
-
 MergeAB = PdfFileMerger()
 MergeAB.append(str(input_pathA))
 MergeAB.append(str(input_pathB))
@@ -24,27 +21,16 @@ MergeBA.append(str(input_pathB))
 MergeBA.append(str(input_pathA))
 
 Dir_selected = filedialog.askdirectory()
+print(Dir_selected)
+print(type(Dir_selected))
+Dir_selected = Dir_selected + "/"
+print(Dir_selected)
+print(type(Dir_selected))
 
+File_Name = input()
 
-with Path("AB.pdf").open(mode="wb") as output_file:
+with Path(Dir_selected+File_Name+".pdf").open(mode="wb") as output_file:
     MergeAB.write(output_file)
 
-with Path("BA.pdf").open(mode="wb") as output_file:
-    MergeBA.write(output_file)
 
-print("TYPE:") 
-print( type(input_pathA))
-print("NAME:") 
-print(input_pathA.name)
-print("OBJECT") 
-print(input_pathA)
-print(DocA.numPages)
-
-print("TYPE:") 
-print( type(input_pathB))
-print("NAME:") 
-print(input_pathB.name)
-print("OBJECT") 
-print(input_pathB)
-print(DocB.numPages)
 
